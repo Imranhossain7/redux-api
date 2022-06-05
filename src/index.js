9bb6b680927eba85bbc679f11741a86a70360d5b
Imran <Imran.ice.nstu12@gmail.com>
2022-06-05 11:39:21 +0600

    Initial set up for redux-toolkit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
-import { createStore, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
 import reportWebVitals from "./reportWebVitals";
 
-// Local imports
 import App from "./App";
-import rootReducer from "./reducers";
+import rootReducer from "./slices";
 
-// Assets
 import "./index.css";
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+const store = configureStore({ reducer: rootReducer });
 
 render(
   <Provider store={store}>
